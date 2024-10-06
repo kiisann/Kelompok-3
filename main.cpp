02708df22d653356f3ff35b932af820e8a91e504
kiisann <fikisulistiawan@gmail.com>
2024-10-06 10:51:58 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,22 +11,22 @@ int bulan, tahun;
     cin >> tahun;
 
       cout << endl;
-      cout <<"Kalender Tahun "<< tahun << endl;
-      cout <<"Pilih bulan: " << endl;
-      cout <<"1. Januari" << endl;
-      cout <<"2. Februari" << endl;
-      cout <<"3. Maret" << endl;
-      cout <<"4. April" << endl;
-      cout <<"5. Mei" << endl;
-      cout <<"6. Juni" << endl;
-      cout <<"7. Juli" << endl;
-      cout <<"8. Agustus" << endl;
-      cout <<"9. September" << endl;
-      cout <<"10. Oktober" << endl;
-      cout <<"11. November" << endl;
-      cout <<"12. Desember" << endl;
-      cout <<"0. Keluar" << endl;
-      cout <<"\nPilihan Anda: " << endl;
+      cout << "Kalender Tahun "<< tahun << endl;
+      cout << "Pilih bulan: " << endl;
+      cout << "1. Januari" << endl;
+      cout << "2. Februari" << endl;
+      cout << "3. Maret" << endl;
+      cout << "4. April" << endl;
+      cout << "5. Mei" << endl;
+      cout << "6. Juni" << endl;
+      cout << "7. Juli" << endl;
+      cout << "8. Agustus" << endl;
+      cout << "9. September" << endl;
+      cout << "10. Oktober" << endl;
+      cout << "11. November" << endl;
+      cout << "12. Desember" << endl;
+      cout << endl;
+      cout <<"Pilihan Anda: " << endl;
       cin >> bulan;
 
       if (bulan >= 1&& bulan <= 12){
